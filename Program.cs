28ca295f0bc60da30a70ec0440b41238e83f8794
Jay Wang <jay.wang@hp.com>
2022-01-04 17:56:15 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() {HostName = "localhost"};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("hello", false, false, false, null);
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (modal, ea) =>
+{
+    var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+    Console.WriteLine($"Message Received: {message}");
+};
+channel.BasicConsume("hello", true, consumer);
+
+
+Console.ReadLine();
